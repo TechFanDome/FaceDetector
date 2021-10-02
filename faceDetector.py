6cff0e50f01a2256f102b3c1ec4160f07de5684d
Jayateerth Dambal <jayateerth.dambal@gmail.com>
2021-10-02 10:56:33 +0530

    Adding some new variables

--- faceDetector.py
+++ faceDetector.py
@@ -2,16 +2,16 @@ import cv2
 import numpy as np 
 import sys
 
-
+### Functioon for Face Detection
 def face_detector():
     cascadePath = 'haarcascade_frontalface_default.xml'
     faceCascade = cv2.CascadeClassifier(cascadePath)
 
-    vid_Cap = cv2.VideoCapture(0)
+    video_Cap = cv2.VideoCapture(0)
 
 
     while True:
-        ret, frame = vid_Cap.read()
+        ret, frame = video_Cap.read()
         grayScaleImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(
             grayScaleImg, scaleFactor=1.5, minNeighbors=5,
@@ -28,7 +28,7 @@ def face_detector():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    vid_Cap.release()
+    video_Cap.release()
     cv2.destroyAllWindows()        
 
 
